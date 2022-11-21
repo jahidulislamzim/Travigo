a2ce21f78f322cafbc758612a00bb51d4ac212c4
jahidulislamzim <jahidulislamzim845@gmail.com>
2022-11-21 20:13:15 +0600

    adding travigo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Navbar, Hero, Footer, Memory, Explore, Advertise, Pricings, Banner, Newslatter } from './components';
+import { hero, navlinks, memory, placesAPI, brands, pricingapi, bannerAPI, footerAPI } from './data/travigodata';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+      <Navbar navlinks={navlinks} />
+        <Hero hero={hero} />
+        <Memory memory={memory} />
+        <Explore title="Explore The Beauty of World" placesAPI={placesAPI} />
+        <Advertise brands={brands} />
+        <Pricings pricingapi={pricingapi} />
+        <Banner bannerAPI={bannerAPI} />
+        <Newslatter />
+      <Footer footerAPI={footerAPI} />
+   </>
   );
-}
+};
 
 export default App;
